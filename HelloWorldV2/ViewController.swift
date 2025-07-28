f3e6745a40eea31e5705bdb36bed05ceb56bee68
whatislove4 <lokotunin12@mail.ru>
2025-07-28 17:07:23 +0300

    Initial Commit

---
+++ HelloWorldV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldV2
+//
+//  Created by Игнат Гончаров on 28.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
